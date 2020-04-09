14b56fd31455602147ba5a9ffdd7975728090b05
springloops <springloops@gmail.com>
2020-04-09 23:32:34 +0900

    container name change to variable"

--- get_jupyter_token.sh
+++ get_jupyter_token.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-container_name=gpu-py3-jupyter
+container_name=$1
 CONTAINER_ID=$(docker ps -aqf "name=${container_name}")
 
 JS=$(docker exec -t ${CONTAINER_ID} jupyter notebook list --json)
 TOKEN=$(echo $JS | jq -r .token)
-
-JT_URL="http://HOST:8888/?token=$TOKEN" 
-
-echo 'go to jupyter'
-echo $JT_URL
+ 
+echo "Jupyter Token $TOKEN"
 
