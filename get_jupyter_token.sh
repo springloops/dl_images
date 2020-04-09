dde9d67492f4fb521e35f8984d19c1b8757cf708
springloops <springloops@gmail.com>
2020-04-09 23:35:05 +0900

    ..

--- get_jupyter_token.sh
+++ get_jupyter_token.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 container_name=$1
+HOST=$2
 CONTAINER_ID=$(docker ps -aqf "name=${container_name}")
 
 JS=$(docker exec -t ${CONTAINER_ID} jupyter notebook list --json)
 TOKEN=$(echo $JS | jq -r .token)
  
 echo "Jupyter Token $TOKEN"
-
+echo "$HOST:8888/lab?token=$TOKEN"
